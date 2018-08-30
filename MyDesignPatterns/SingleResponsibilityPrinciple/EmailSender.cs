a2fbd8843fee581a8541f9cc97f0262b9a429cfe
bin0m <levitg@gmail.com>
2018-08-30 18:23:48 +0300

    Single Responsibility Principle

---
+++ MyDesignPatterns/SingleResponsibilityPrinciple/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDesignPatterns.SingleResponsibilityPrinciple
+{
+    interface IEmailSender
+    {
+        void SendEmail(string emailAdress, string subject, string content);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        public void SendEmail(string emailAdress, string subject, string content)
+        {
+            // Sends email
+        }
+    }
+
+    public class CustomerRepository
+    {
+        public Customer GetCustomer(string id)
+        {
+            // logic load customer from Database
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmailContentBuilder
+    {
+        public string GetEmailContent(Customer customerDetails)
+        {
+            // logic to build email content
+            throw new NotImplementedException();
+        }
+    }   
+}
---
+++ MyDesignPatterns/SingleResponsibilityPrinciple/EmailSenderBefore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyDesignPatterns.SingleResponsibilityPrinciple
+{
+    public struct Customer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+
+    public class EmailSenderBefore
+    {
+        public void SendEmail(string customerID,
+                             string emailNotificationType)
+        {
+            //STEP1: load customer details
+            //STEP2: get email content
+            //STEP3: send email (using SmtpClient class)
+        }
+
+        public string GetEmailContent(Customer customer,
+                       string emailNotificationType)
+        {
+            // Build the email notification content
+            throw new NotImplementedException();
+        }
+    }
+
+    /*
+The EmailSender class can go under changes because any of the below reasons:
+1. Changes in the way, you are loading customer details.
+2. Changes to the email content because of requirement enhancements / changes.
+3. If there any change in the way you are sending the email instead of using SmtpClient class or something like that.
+     */
+}
